910b726ef0f9fee2d1048b144f8169bf2ec338b9
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-02-19 12:23:11 +0100

    ensure that all examples in bug_spec get run

--- spec/bug_spec.rb
+++ spec/bug_spec.rb
@@ -3,66 +3,35 @@ require 'spec_helper'
 
 describe BlueCloth, 'bugs' do
 
-	### :TODO: Add more documents and test their transforms.
+	include SampleLoader
 
-  it "regexp engine overflow" do
-    pending
-		contents = File::read( File::join(BaseDir,"tests/data/re-overflow.txt") )
-		bcobj = BlueCloth::new( contents )
-
-		assert_nothing_raised {
-			bcobj.to_html
-		}
+  it "should not regexp engine overflow" do
+		contents = File::read sample_file("re-overflow.txt")
+		lambda { markdown(contents) }.should_not raise_error
 	end
 	
-  it "regexp engine overflow 2" do
-    pending
-		contents = File::read( File::join(BaseDir,"tests/data/re-overflow2.txt") )
-		bcobj = BlueCloth::new( contents )
-
-		assert_nothing_raised {
-			bcobj.to_html
-		}
+  it "should not regexp engine overflow" do
+		contents = File::read sample_file("re-overflow2.txt")
+		lambda { markdown(contents) }.should_not raise_error
 	end
 	
   it "2 character bold asterisks" do
-    pending
-		html = nil
-		str = BlueCloth::new( "**aa**" )
-		assert_nothing_raised do
-			html = str.to_html
-		end
-
-		assert_equal "<p><strong>aa</strong></p>", html
+		markdown("**aa**").should == "<p><strong>aa</strong></p>"
 	end
 
   it "should correctly emphasize two character words with asterisks" do
-    BlueCloth::new("*aa*").to_html.should == "<p><em>aa</em></p>"
+    markdown("*aa*").should == "<p><em>aa</em></p>"
 	end
 	
   it "should correctly emphasize two character words with underscores" do
-    BlueCloth::new("_aa_").to_html.should == "<p><em>aa</em></p>"
+    markdown("_aa_").should == "<p><em>aa</em></p>"
 	end
 
-  it "2 character emphasis underscores" do
-    pending
-		html = nil
-		str = BlueCloth::new( "_aa_" )
-		assert_nothing_raised do
-			html = str.to_html
-		end
-
-		assert_equal "<p><em>aa</em></p>", html
-	end
-
-	it "ruby with warnings enabled causes ArgumentError" do
-    pending
+	it "should not raise ArgumentError when ruby enables warnings" do
 		oldverbose = $VERBOSE
     begin
-      assert_nothing_raised do
-        $VERBOSE = true
-        BlueCloth.new( "*woo*" ).to_html
-      end
+      $VERBOSE = true
+      lambda { markdown("*woo*") }.should_not raise_error
     ensure
       $VERBOSE = oldverbose
     end
